a3b8ae213376636cd6ff061a1d0dba097fc90084
wan-huang <wan.huang@mail.mcgill.ca>
2023-06-09 19:31:59 -0400

    Added Contact Us Page

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path(route='about/', view=views.about, name='about'),
 
     # path for contact us view
+    path(route='contact/', view=views.contact, name='contact'),
 
     # path for registration
 
